53a18489e4d962d3253ab420ceddec2c81400b90
kenify <582826210@qq.com>
2020-10-09 10:39:01 +0800

    style:添加README文件

---
+++ README.md
@@ -0,0 +1,2 @@
+#go-websocket
+基于go官方库的websocket聊天室
---
+++ src/components/main.go
@@ -0,0 +1,13 @@
+package main
+
+const PostData = {
+	username: "张三",
+	password: "123456"
+}
+
+func main() {
+	axios.post("/register",PostData).then((response) => {
+		conslo
+	})
+	//fmt.Print("123456")
+}
